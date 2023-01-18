12c03a6ddfbe29d66b6ca50ccbeec689caf06d1d
Caleb Short <calebshort4697@gmail.com>
2023-01-18 14:35:18 -0500

    Basic connection to backend and elasticsearch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,63 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('singleRecordForm') singleRecordForm: NgForm|undefined;
+  private _fetchedRecord: object|undefined;
+
+  get fetchedRecord() {
+    return JSON.stringify(this._fetchedRecord);
+  }
+
+  @ViewChild('publishedRecordsForm') publishedRecordsForm: NgForm|undefined;
+  private _fetchedDatasetRecords: object|undefined;
+
+  get fetchedDatasetRecords() {
+    return JSON.stringify(this._fetchedDatasetRecords);
+  }
+
+  @ViewChild('searchRecordsForm') searchRecordsForm: NgForm|undefined;
+  private _searchedDatasetRecords: object|undefined;
+
+  get searchedDatasetRecords() {
+    return JSON.stringify(this._searchedDatasetRecords);
+  }
+
+  constructor(private http: HttpClient) {}
+
+  fetchRecord() {
+    const uuid = this.singleRecordForm?.value['uuid'];
+    console.log('fetch record called with uuid: ' + uuid);
+    this.http.get(environment.backend_url +  '/record/' + uuid + '/latest_persisted')
+      .subscribe(response => {
+        this._fetchedRecord = response;
+      })
+  }
+
+  fetchPublishedRecords() {
+    const uuid = this.publishedRecordsForm?.value['uuid'];
+    const version_name = this.publishedRecordsForm?.value['version_name'];
+    console.log('fetch dataset called with uuid ' + uuid + ' and version name ' + version_name);
+    this.http.get('http://localhost:3000/dataset/' + uuid + '/published/' + version_name + '/records')
+      .subscribe(response => {
+        this._fetchedDatasetRecords = response;
+      })
+  }
+
+  searchPublishedRecords() {
+    const uuid = this.searchRecordsForm?.value['uuid'];
+    const version_name = this.searchRecordsForm?.value['version_name'];
+    const key = this.searchRecordsForm?.value['key'];
+    const value = this.searchRecordsForm?.value['value'];
+    console.log('search dataset called with uuid ' + uuid + ' and version name ' + version_name
+      + " and key " + key + " and value " + value);
+    let url = 'http://localhost:3000/dataset/' + uuid + '/published/' + version_name + '/search_records';
+    if(key && value) {
+      url += "?" + key + "=" + value;
+    }
+    this.http.get(url)
+      .subscribe(response => {
+        this._searchedDatasetRecords = response;
+      })
+  }
 
 }
